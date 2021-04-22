45ec2443f37a09386047fc864d7228d528913ab2
Emilio Bonmatí <emilre68@gmail.com>
2021-04-22 11:49:26 +0200

    second commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavComponent,
+    FooterComponent
+  ],
+  exports:[
+    NavComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
